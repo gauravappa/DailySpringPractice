95bd099dfeceab492df20b1d95bd3f91cc7f75ef
gauravappa <gauravappagga@gmail.com>
2025-09-05 11:32:50 +0530

    first commit - spring boot project with web,security,jpa dependency

---
+++ src/test/java/com/example/dailyspringpractice/DailySpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailyspringpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailySpringPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
